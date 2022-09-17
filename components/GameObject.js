a5061f2ea2f82eed6f1d66e6af8b2526220bda40
Teerasit <43192931+Aicl0ud@users.noreply.github.com>
2022-09-18 00:33:35 +0700

    chore: split player from overworld to object

---
+++ components/GameObject.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Sprite from "../components/Sprite.js";
+class GameObject extends Component {
+  constructor(config) {
+    super(config);
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: config.src || "/images/characters/player/mPlayer_[human].png",
+    });
+  }
+}
+
+export default GameObject;
--- components/overworld.js
+++ components/overworld.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import GameObject from "../components/GameObject.js";
+
 class Overworld extends Component {
   constructor(props) {
     super(props);
@@ -7,44 +9,54 @@ class Overworld extends Component {
     this.ctx = props.element.querySelector("canvas").getContext("2d");
   }
   init() {
-    console.log("True");
     const image = new Image();
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0);
     };
     image.src = "/images/maps/main/Main.gif";
-    const x = 6;
-    const y = 6;
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    shadow.src = "/images/characters/shadow.png";
-    const player = new Image();
-    player.onload = () => {
-      this.ctx.drawImage(
-        player,
-        0, //left cut
-        0, //top cut,
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    player.src = "/images/characters/player/mPlayer_[human].png";
+
+    //Place some Game Objects!
+    const player = new GameObject({
+      x: 6,
+      y: 5,
+    });
+
+    setTimeout(() => {
+      player.sprite.draw(this.ctx);
+    }, 200);
+
+    // const x = 6;
+    // const y = 6;
+    // const shadow = new Image();
+    // shadow.onload = () => {
+    //   this.ctx.drawImage(
+    //     shadow,
+    //     0, //left cut
+    //     0, //top cut,
+    //     32, //width of cut
+    //     32, //height of cut
+    //     x * 16 - 8,
+    //     y * 16 - 18,
+    //     32,
+    //     32
+    //   );
+    // };
+    // shadow.src = "/images/characters/shadow.png";
+    // const player = new Image();
+    // player.onload = () => {
+    //   this.ctx.drawImage(
+    //     player,
+    //     0, //left cut
+    //     0, //top cut,
+    //     32, //width of cut
+    //     32, //height of cut
+    //     x * 16 - 8,
+    //     y * 16 - 18,
+    //     32,
+    //     32
+    //   );
+    // };
+    // player.src = "/images/characters/player/mPlayer_[human].png";
   }
 }
 
