aa296776da5593cf1017412ac0bf585b7a994915
Teerasit <43192931+Aicl0ud@users.noreply.github.com>
2022-09-19 23:24:24 +0700

    chore: update animation

--- components/Maps.js
+++ components/Maps.js
@@ -7,14 +7,14 @@ const Maps = () => {
       lowerSrc: "/images/maps/main/Main.gif",
       upperSrc: "/images/maps/main/Main-Top.png",
       gameObjects: {
-        hero: new Person({
+        player: new Person({
           isPlayerControlled: true,
-          x: utils.withGrid(5),
-          y: utils.withGrid(6),
+          x: utils.withGrid(6),
+          y: utils.withGrid(5),
         }),
         npc1: new Person({
-          x: utils.withGrid(7),
-          y: utils.withGrid(9),
+          x: utils.withGrid(4),
+          y: utils.withGrid(10),
           src: "/images/characters/player/fPlayer_[human].png",
         }),
       },
--- components/Sprite.js
+++ components/Sprite.js
@@ -22,13 +22,89 @@ class Sprite extends Component {
 
     //Configure Animation & Initial State
     this.animations = config.animations || {
-      idleDown: [[0, 0]],
+      "idle-down": [[0, 0]],
+      "idle-right": [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+        [3, 1],
+      ],
+      "idle-up": [[0, 2]],
+      "idle-left": [
+        [4, 1],
+        [5, 1],
+        [6, 1],
+        [7, 1],
+      ],
+      "walk-down": [[0, 0]],
+      "walk-right": [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [0, 3],
+        [1, 3],
+        [2, 3],
+        [3, 3],
+      ],
+      "walk-up": [[0, 0]],
+      "walk-left": [
+        [4, 2],
+        [5, 2],
+        [6, 2],
+        [7, 2],
+        [4, 3],
+        [5, 3],
+        [6, 3],
+        [7, 3],
+      ],
     };
-    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimation = "idle-right"; // config.currentAnimation || "idle-down";
     this.currentAnimationFrame = 0;
+
+    this.animationFrameLimit = config.animationFrameLimit || 8;
+    this.animationFrameProgress = this.animationFrameLimit;
 
     //Reference the game object
     this.gameObject = config.gameObject;
+  }
+
+  get frame() {
+    return this.animations[this.currentAnimation][this.currentAnimationFrame];
+    // return this.animations["idle-down"][0];
+  }
+
+  setAnimation(key) {
+    // console.log(key);
+    if (this.currentAnimation !== key) {
+      if (key.includes("up")) {
+        key = key.replace("up", this.currentAnimation.split("-")[1]);
+        console.log(key);
+      } else if (key.includes("down")) {
+        key = key.replace("down", this.currentAnimation.split("-")[1]);
+        console.log(key);
+      } else {
+        this.currentAnimation = key;
+        this.currentAnimationFrame = 0;
+        this.animationFrameProgress = this.animationFrameLimit;
+      }
+    }
+  }
+
+  updateAnimationProgress() {
+    //Downtick frame progress
+    if (this.animationFrameProgress > 0) {
+      this.animationFrameProgress -= 1;
+      return;
+    }
+
+    //Reset the counter
+    this.animationFrameProgress = this.animationFrameLimit;
+    this.currentAnimationFrame += 1;
+
+    if (this.frame === undefined) {
+      this.currentAnimationFrame = 0;
+    }
   }
 
   draw(ctx) {
@@ -37,7 +113,12 @@ class Sprite extends Component {
 
     this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
 
-    this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
+    const [frameX, frameY] = this.frame;
+
+    this.isLoaded &&
+      ctx.drawImage(this.image, frameX * 32, frameY * 32, 32, 32, x, y, 32, 32);
+
+    this.updateAnimationProgress();
   }
 }
 
