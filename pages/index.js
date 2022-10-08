acbbc59c15d8bf0c3a01febe6c4182d76e91546e
Teerasit <43192931+Aicl0ud@users.noreply.github.com>
2022-10-08 21:24:17 +0700

    chore: change tsx to js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,72 @@
-import xw from 'xwind'
-import ButtonReact from '../components/ButtonReact'
-import ButtonStyled from '../components/ButtonStyled'
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Overworld from "../components/Overworld.js";
+import GameObject from "../components/GameObject.js";
+import Maps from "../components/Maps.js";
+import Keyboard from "../components/Keyboard.js";
 
-const Index = () => (
-  <div css={xw`grid justify-center items-center h-screen space-y-20`}>
-    <div css={xw`space-y-20`}>
-      <ButtonReact>@emotion/react</ButtonReact>
-      <ButtonStyled>@emotion/styled</ButtonStyled>
+const Index = () => {
+  useEffect(() => {
+    init();
+  }, []);
+
+  function init() {
+    Maps();
+    const overworld = new Overworld({
+      element: document.querySelector(".game-container"),
+    });
+    overworld.init();
+  }
+
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+        <div className="game-container relative">
+          <canvas
+            className="game-canvas bg-[#353540]"
+            width={"320"}
+            height={"180"}
+          ></canvas>
+          <div
+            id="position"
+            className="absolute bottom-0 right-0 mr-1 text-white"
+          ></div>
+
+          <div className="box wooden transition transition-all duration-500">
+            <div className="content">
+              <div
+                className="absolute top-0 right-0 font-bold text-red-600 cursor-pointer"
+                onClick={(e) => {
+                  document.querySelector(".box").classList.add("opacity-0");
+                }}
+              >
+                X
+              </div>
+              <div className="title">Get started</div>
+              <hr className="mt-1 mb-2" />
+              <div className="msg">
+                <div className="flex items-center gap-0.5">
+                  use <Keyboard text={"w,a,s,d"}></Keyboard> or{" "}
+                  <Keyboard text={"↑,↓,→,←"}></Keyboard> to move{" "}
+                </div>
+                <div className="flex items-center gap-0.5">
+                  use <Keyboard text={"z,x"}></Keyboard> or{" "}
+                  <Keyboard word={" ↵ ,spacebar"}></Keyboard> to interact{" "}
+                </div>
+                <div>Then go 3 red marks 🔻 to know me more</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  </div>
-)
+  );
+};
 
-export default Index
+export default Index;
